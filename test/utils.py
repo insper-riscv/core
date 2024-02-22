e7d0165f6b42cac62999052b2e26aed0c883cbb5
FelixLuciano <felix.lucianodias@gmail.com>
2024-02-22 11:41:42 -0300

    Add test case building and running functionality tests

--- test/utils.py
+++ test/utils.py
@@ -1,3 +1,4 @@
+from pickle import BUILD
 import typing as T
 
 import cocotb.binary
@@ -6,15 +7,50 @@ import cocotb.log
 import cocotb.runner
 
 
+_TESTCASE_TYPE = T.Callable[["DUT"], None] | T.Sequence[T.Callable[["DUT"], None]]
+_BUILT: T.List[str] = []
+
 runner = cocotb.runner.get_runner("ghdl")
 
 
 class DUT(T.Type[cocotb.handle.HierarchyObject]):
+    CHILDREN: T.List[T.Type["DUT"]] = []
     _log: T.Any
-    pass
 
     class Input_pin(T.Type[cocotb.handle.ModifiableObject]):
         value: cocotb.binary.BinaryValue
 
     class Output_pin(T.Type[cocotb.handle.ModifiableObject]):
-        value: T.Final[cocotb.binary.BinaryValue]  # type: ignore
+        value: T.Final[cocotb.binary.BinaryValue] # type: ignore
+
+    @staticmethod
+    def _get_testcase_names(case: _TESTCASE_TYPE):
+        if isinstance(case, list):
+            return [case.__name__ for case in case]
+        else:
+            return case.__name__
+    
+    @classmethod
+    def build_vhd(cls):
+        for child in cls.CHILDREN:
+            if child.__name__ in _BUILT:
+                continue
+
+            child.build_vhd()
+            _BUILT.append(child.__name__)
+
+        runner.build(
+            vhdl_sources=[f"src/{cls.__name__}.vhd"],
+            hdl_toplevel=cls.__name__.lower(),
+            always=True,
+        )
+
+    @classmethod
+    def test_with(cls, testcase: _TESTCASE_TYPE, parameters: T.Mapping[str, object] | None= None):
+        runner.test(
+            hdl_toplevel=cls.__name__.lower(),
+            test_module="test_" + cls.__name__,
+            testcase=DUT._get_testcase_names(testcase),
+            parameters=parameters, # type: ignore
+            hdl_toplevel_lang="vhdl",
+        )
