9605eeb9fbe327a484a71959b4c23b6f70df4606
Tiago Seixas <tvseixas@gmail.com>
2024-02-27 21:04:48 -0300

    correcting test for GENERIC_EDGE_DETECTOR

--- test/test_GENERIC_EDGE_DETECTOR.py
+++ test/test_GENERIC_EDGE_DETECTOR.py
@@ -17,21 +17,19 @@ class GENERIC_EDGE_DETECTOR(utils.DUT):
 
 @cocotb.test()
 async def tb_GENERIC_EDGE_DETECTOR(dut: GENERIC_EDGE_DETECTOR):
-    values_source_rise = ["0", "1"]
-    values_pulse_rise = ["0", "0"]
-    values_source_fall = ["0", "1"]
-    values_pulse_fall = ["1", "0"]
+    values_source_rise = ["0", "1", "0", "0",]
+    values_pulse_rise = ["0", "0", "0", "1"]
+    values_source_fall = ["0", "1", "0", "0",]
+    values_pulse_fall = ["0", "0", "0", "1"]
     clock = Clock(dut.clock, 20000, units="ns")
 
+    cocotb.start_soon(clock.start(start_high=False))
     for index, (source_rise, pulse) in enumerate(
         zip(values_source_rise, values_pulse_rise)
     ):
         dut.source.value = BinaryValue(source_rise)
-        cocotb.start_soon(clock.start(start_high=False))
 
         await RisingEdge(dut.clock)
-
-        clock = Clock(dut.clock, 20000, units="ns")
 
         condition = dut.pulse.value.binstr == pulse
 
@@ -43,17 +41,15 @@ async def tb_GENERIC_EDGE_DETECTOR(dut: GENERIC_EDGE_DETECTOR):
         assert (
             condition
         ), f"Error in test rise {index}: clock={clock} source_rise={source_rise}"
-        await Timer(Decimal(20000), units="ns")
+        await FallingEdge(dut.clock)
 
+    cocotb.start_soon(clock.start(start_high=True))
     for index, (source_fall, pulse) in enumerate(
         zip(values_source_fall, values_pulse_fall)
     ):
         dut.source.value = BinaryValue(source_fall)
-        cocotb.start_soon(clock.start(start_high=True))
 
         await FallingEdge(dut.clock)
-
-        clock = Clock(dut.clock, 20000, units="ns")
 
         condition = dut.pulse.value.binstr == pulse
 
@@ -65,7 +61,7 @@ async def tb_GENERIC_EDGE_DETECTOR(dut: GENERIC_EDGE_DETECTOR):
         assert (
             condition
         ), f"Error in test fall {index}: clock={clock} source_fall={source_fall}"
-        await Timer(Decimal(20000), units="ns")
+        await RisingEdge(dut.clock)
 
 
 def test_GENERIC_EDGE_DETECTOR():
