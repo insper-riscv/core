814c05ff820f54bc069f00c1eae4b50f0db6e11a
Tiago Seixas <tvseixas@gmail.com>
2024-03-04 19:50:54 -0300

    implementing MODULE_ID and MODULE_IF(minus ROM)

--- src/MODULE_ID.vhd
+++ src/MODULE_ID.vhd
@@ -6,11 +6,57 @@ use WORK.TOP_LEVEL_CONSTANTS.ALL;
 
 entity MODULE_ID is
 
+    generic (
+        DATA_WIDTH_0  : natural := DATA_WIDTH;
+        ADDRESS_WIDTH : natural := 5
+    );
+
+    port (
+        enable              : in  std_logic := '0';
+        address_destination : in  std_logic_vector((ADDRESS_WIDTH - 1) downto 0);
+        address_source_1    : in  std_logic_vector((ADDRESS_WIDTH - 1) downto 0);
+        address_source_2    : in  std_logic_vector((ADDRESS_WIDTH - 1) downto 0);
+        data_destination    : in  std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        instruction         : in  std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        pc_out              : in  std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        id_source           : out std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        immediate_source    : out std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        data_source_1       : out std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        data_source_2       : out std_logic_vector((DATA_WIDTH_0 - 1) downto 0)
+    );
+
 end entity;
 
 architecture RTL of MODULE_ID is
-        
+        signal immediate_shift : std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
 begin   
+    SIGNAL_EXTENDER : entity WORK.GENERIC_SIGNAL_EXTENDER
+        port map (
+            source => immediate_shift,
+            enable_unsigned => '0',
+            destination => immediate_source
+        );
+
+    immediate_shift <= immediate_source(29 downto 0) & "00";
+
+    ADDER : entity WORK.GENERIC_ADDER
+        port map (
+            source_1 => immediate_shift,
+            source_2 => pc_out,
+            destination => id_source
+        );
+
+    REGISTER_BANK : entity WORK.GENERIC_REGISTERS_BANK
+        port (
+        enable              => enable,
+        address_destination => address_destination,
+        address_source_1    => address_source_1,
+        address_source_2    => address_source_2,
+        data_destination    => data_destination,
+        data_source_1       => data_source_1,
+        data_source_2       => data_source_2
+    );
+
     
 
 end architecture;
--- src/MODULE_IF.vhd
+++ src/MODULE_IF.vhd
@@ -6,11 +6,46 @@ use WORK.TOP_LEVEL_CONSTANTS.ALL;
 
 entity MODULE_IF is
 
+    generic (
+        DATA_WIDTH_0  : natural := DATA_WIDTH
+    );
+
+    port (
+        id_source   : in  std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        selector    : in  std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        destination : out std_logic_vector((DATA_WIDTH_0 - 1) downto 0)
+    );
+
 end entity;
 
 architecture RTL of MODULE_IF is
-        
+        signal adder_out : std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        signal mux_out   : std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
+        signal pc_out    : std_logic_vector((DATA_WIDTH_0 - 1) downto 0);
 begin   
     
+    MUX_REGISTER_ALU_1 : entity WORK.GENERIC_MUX_2X1
+        port map (
+            source_1 => id_source,
+            source_2 => adder_out,
+            selector => selector,
+            destination => mux_out
+        );
+    
+    PC : entity WORK.GENERIC_REGISTER
+        port map (
+            clear => clear,
+            enable => enable,
+            source => source,
+            destination => pc_out
+        );
+
+    ADDER : entity WORK.GENERIC_ADDER
+        port map (
+            source_1 => pc_out,
+            source_2 => (others => '0') & "100",
+            destination => adder_out
+        );
+
 
 end architecture;
