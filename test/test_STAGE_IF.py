91d9bb76f4c6a27ae5061454ed9ccd5967c13871
FelixLuciano <felix.lucianodias@gmail.com>
2024-03-10 22:04:06 -0300

    Add MODULE_ROM to STAGE_IF.CHILDREN and update test_STAGE_IF_syntesis()

--- test/test_STAGE_IF.py
+++ test/test_STAGE_IF.py
@@ -1,25 +1,23 @@
 from decimal import Decimal
 
+import pytest
 import cocotb
 from cocotb.binary import BinaryValue
 from cocotb.triggers import Timer
 
 import utils
 from test_MODULE_PC import MODULE_PC
+from test_MODULE_ROM import MODULE_ROM
 
 
 class STAGE_IF(utils.DUT):
-    CHILDREN = [MODULE_PC]
+    CHILDREN = [MODULE_PC, MODULE_ROM]
 
 
-@cocotb.test()
-async def tb_STAGE_IF(dut: STAGE_IF):
-    pass
-
-
-def test_STAGE_IF():
-    STAGE_IF.test_with(tb_STAGE_IF)
+def test_STAGE_IF_syntesis():
+    STAGE_IF.build_vhd()
+    STAGE_IF.build_netlistsvg()
 
 
 if __name__ == "__main__":
-    test_STAGE_IF()
+    pytest.main(["-k", f"test_STAGE_IF"])
