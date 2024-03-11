6d26faa89a93e90077bbd7a58af92e2f66f18f2b
FelixLuciano <felix.lucianodias@gmail.com>
2024-03-10 22:02:32 -0300

    Refactor tb_GENERIC_RAM function in test_GENERIC_RAM.py

--- test/test_GENERIC_RAM.py
+++ test/test_GENERIC_RAM.py
@@ -20,28 +20,10 @@ class GENERIC_RAM(utils.DUT):
 
 
 @cocotb.test()
-async def tb_GENERIC_RAM(dut: GENERIC_RAM):
-    values_enable = [
-        "0",
-        "1",
-        "1",
-        "1",
-        "1",
-    ]
-    values_enable_read = [
-        "1",
-        "0",
-        "1",
-        "0",
-        "1",
-    ]
-    values_enable_write = [
-        "1",
-        "1",
-        "0",
-        "1",
-        "0",
-    ]
+async def tb_GENERIC_RAM_case_1(dut: GENERIC_RAM):
+    values_enable = ["0", "1", "1", "1", "1"]
+    values_enable_read = ["1", "0", "1", "0", "1"]
+    values_enable_write = ["1", "1", "0", "1", "0"]
     values_address = [
         "00000000000000000000000000000001",
         "00000000000000000000000000000001",
@@ -62,41 +44,48 @@ async def tb_GENERIC_RAM(dut: GENERIC_RAM):
         "00001111000011110000111100001111",
         "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ",
         "11110000111100001111000011110000",
-
     ]
     clock = Clock(dut.clock, 20000, units="ns")
 
-    for index, (enable, enable_read, enable_write, address,
-        source, destination) in enumerate(
-        zip(values_enable, values_enable_read, values_enable_write, values_address, values_source, values_destination)
+    cocotb.start_soon(clock.start(start_high=False))
+
+    for index, (
+        enable,
+        enable_read,
+        enable_write,
+        address,
+        source,
+        destination,
+    ) in enumerate(
+        zip(
+            values_enable,
+            values_enable_read,
+            values_enable_write,
+            values_address,
+            values_source,
+            values_destination,
+        )
     ):
         dut.enable.value = BinaryValue(enable)
         dut.enable_read.value = BinaryValue(enable_read)
         dut.enable_write.value = BinaryValue(enable_write)
         dut.address.value = BinaryValue(address)
         dut.source.value = BinaryValue(source)
-        cocotb.start_soon(clock.start(start_high=False))
 
         await RisingEdge(dut.clock)
-
         await Timer(Decimal(20000), units="ns")
-
-        condition = dut.destination.value.binstr == destination
-
-        if not condition:
-            dut._log.error(
-                f"Expected value: {destination} Obtained value: {dut.destination.value.binstr}"
-            )
-
-        assert (
-            condition
-        ), f"Error in test {index}: enable={enable} enable_read={enable_read} enable_write={enable_write} address={address} source={source}"
+        utils.assert_output(dut.destination, destination, f"At clock {index}.")
         await Timer(Decimal(20000), units="ns")
 
 
-def test_GENERIC_RAM():
-    GENERIC_RAM.test_with(tb_GENERIC_RAM)
+def test_GENERIC_RAM_syntesis():
+    GENERIC_RAM.build_vhd()
+    GENERIC_RAM.build_netlistsvg()
+
+
+def test_GENERIC_RAM_case_1():
+    GENERIC_RAM.test_with(tb_GENERIC_RAM_case_1)
 
 
 if __name__ == "__main__":
-    test_GENERIC_RAM()
+    pytest.main(["-k", f"test_GENERIC_RAM"])
