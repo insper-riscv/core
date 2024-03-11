7a8fbeaa5b421380d8994c43eafca91c34969c2b
FelixLuciano <felix.lucianodias@gmail.com>
2024-03-10 22:03:03 -0300

    Update test_MODULE_REGISTER_FILE.py with new imports and test_MODULE_REGISTER_FILE_syntesis() function

--- test/test_MODULE_REGISTER_FILE.py
+++ test/test_MODULE_REGISTER_FILE.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 
+import pytest
 import cocotb
 from cocotb.binary import BinaryValue
 from cocotb.triggers import Timer
@@ -7,21 +8,17 @@ from cocotb.triggers import Timer
 import utils
 from test_GENERIC_SIGNAL_EXTENDER import GENERIC_SIGNAL_EXTENDER
 from test_GENERIC_ADDER import GENERIC_ADDER
-from test_GENERIC_REGISTER_FILE import GENERIC_REGISTER_FILE
+from test_RV32I_REGISTER_FILE import RV32I_REGISTER_FILE
 
 
 class MODULE_REGISTER_FILE(utils.DUT):
-    CHILDREN = [GENERIC_SIGNAL_EXTENDER, GENERIC_ADDER, GENERIC_REGISTER_FILE]
+    CHILDREN = [GENERIC_SIGNAL_EXTENDER, GENERIC_ADDER, RV32I_REGISTER_FILE]
 
 
-@cocotb.test()
-async def tb_MODULE_REGISTER_FILE(dut: MODULE_REGISTER_FILE):
-    pass
-
-
-def test_MODULE_REGISTER_FILE():
-    MODULE_REGISTER_FILE.test_with(tb_MODULE_REGISTER_FILE)
+def test_MODULE_REGISTER_FILE_syntesis():
+    MODULE_REGISTER_FILE.build_vhd()
+    MODULE_REGISTER_FILE.build_netlistsvg()
 
 
 if __name__ == "__main__":
-    test_MODULE_REGISTER_FILE()
+    pytest.main(["-k", f"test_MODULE_REGISTER_FILE"])
