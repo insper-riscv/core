0a410dee644bab5fb2a28e0242c6dd0a499479d5
FelixLuciano <felix.lucianodias@gmail.com>
2024-03-10 22:02:08 -0300

    Refactor GENERIC_ADDER test cases and add synthesis and additional test cases

--- test/test_GENERIC_ADDER.py
+++ test/test_GENERIC_ADDER.py
@@ -15,48 +15,73 @@ class GENERIC_ADDER(utils.DUT):
 
 
 @cocotb.test()
-async def tb_GENERIC_ADDER(dut: "GENERIC_ADDER"):
-    values_source_1 = [
-        "00000000000000000000000000000000",
-        "10101010101010101010101010101010",
-        "00101010101010101010101010101010",
-        "11111111111111111111111111111110",
-    ]
-    values_source_2 = [
-        "00000000000000000000000000000000",
-        "01010101010101010101010101010101",
-        "00101010101010101010101010101010",
-        "00000000000000000000000000000001",
-    ]
-    values_destination = [
-        "00000000000000000000000000000000",
-        "11111111111111111111111111111111",
-        "01010101010101010101010101010100",
-        "11111111111111111111111111111111",
-    ]
+async def tb_GENERIC_ADDER_case_1(dut: "GENERIC_ADDER"):
+    dut.source_1.value = BinaryValue("00000000000000000000000000000000")
+    dut.source_2.value = BinaryValue("00000000000000000000000000000000")
 
-    for index, (source_1, source_2, destination) in enumerate(
-        zip(values_source_1, values_source_2, values_destination)
-    ):
-        dut.source_1.value = BinaryValue(source_1)
-        dut.source_2.value = BinaryValue(source_2)
+    await Timer(Decimal(1), units="ns")
 
-        await Timer(Decimal(1), units="ns")
+    utils.assert_output(dut.destination, "00000000000000000000000000000000")
 
-        condition = dut.destination.value.binstr == destination
-
-        if not condition:
-            dut._log.error(
-                f"Expected value: {destination} Obtained value: {dut.destination.value.binstr}"
-            )
-
-        assert condition, f"Error in test {index}: inA={source_1} inB={source_2}"
-        await Timer(Decimal(1), units="ns")
+    await Timer(Decimal(1), units="ns")
 
 
-def test_GENERIC_ADDER():
-    GENERIC_ADDER.test_with(tb_GENERIC_ADDER)
+@cocotb.test()
+async def tb_GENERIC_ADDER_case_2(dut: "GENERIC_ADDER"):
+    dut.source_1.value = BinaryValue("10101010101010101010101010101010")
+    dut.source_2.value = BinaryValue("01010101010101010101010101010101")
+
+    await Timer(Decimal(1), units="ns")
+
+    utils.assert_output(dut.destination, "11111111111111111111111111111111")
+
+    await Timer(Decimal(1), units="ns")
+
+
+@cocotb.test()
+async def tb_GENERIC_ADDER_case_3(dut: "GENERIC_ADDER"):
+    dut.source_1.value = BinaryValue("00101010101010101010101010101010")
+    dut.source_2.value = BinaryValue("00101010101010101010101010101010")
+
+    await Timer(Decimal(1), units="ns")
+
+    utils.assert_output(dut.destination, "01010101010101010101010101010100")
+
+    await Timer(Decimal(1), units="ns")
+
+
+@cocotb.test()
+async def tb_GENERIC_ADDER_case_4(dut: "GENERIC_ADDER"):
+    dut.source_1.value = BinaryValue("11111111111111111111111111111110")
+    dut.source_2.value = BinaryValue("00000000000000000000000000000001")
+
+    await Timer(Decimal(1), units="ns")
+
+    utils.assert_output(dut.destination, "11111111111111111111111111111111")
+
+    await Timer(Decimal(1), units="ns")
+
+
+def test_GENERIC_ADDER_syntesis():
+    GENERIC_ADDER.build_vhd()
+    GENERIC_ADDER.build_netlistsvg()
+
+
+def test_GENERIC_ADDER_case_1():
+    GENERIC_ADDER.test_with(tb_GENERIC_ADDER_case_1)
+
+
+def test_GENERIC_ADDER_case_2():
+    GENERIC_ADDER.test_with(tb_GENERIC_ADDER_case_2)
+
+
+def test_GENERIC_ADDER_case_3():
+    GENERIC_ADDER.test_with(tb_GENERIC_ADDER_case_3)
+
+
+def test_GENERIC_ADDER_case_4():
+    GENERIC_ADDER.test_with(tb_GENERIC_ADDER_case_4)
 
 
 if __name__ == "__main__":
-    test_GENERIC_ADDER()
+    pytest.main(["-k", f"test_GENERIC_ADDER"])
