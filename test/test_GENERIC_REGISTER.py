8058fb2d9c0cd866b310bda04774aeed05b40854
FelixLuciano <felix.lucianodias@gmail.com>
2024-03-10 22:02:35 -0300

    Refactor test_GENERIC_REGISTER function

--- test/test_GENERIC_REGISTER.py
+++ test/test_GENERIC_REGISTER.py
@@ -18,8 +18,7 @@ class GENERIC_REGISTER(utils.DUT):
 
 
 @cocotb.test()
-async def tb_GENERIC_REGISTER(dut: GENERIC_REGISTER):
-    clock = Clock(dut.clock, 20000, units="ns")
+async def tb_GENERIC_REGISTER_case_1(dut: GENERIC_REGISTER):
     values_clear = ["0", "0", "1", "0", "0"]
     values_enable = ["1", "0", "0", "1", "1"]
     values_source = [
@@ -36,6 +35,9 @@ async def tb_GENERIC_REGISTER(dut: GENERIC_REGISTER):
         "11111111111111111111111111111111",
         "00000000000000000000000000000000",
     ]
+    clock = Clock(dut.clock, 20000, units="ns")
+
+    cocotb.start_soon(clock.start(start_high=False))
 
     for index, (clear, enable, source, destination) in enumerate(
         zip(values_clear, values_enable, values_source, values_destination)
@@ -43,28 +45,21 @@ async def tb_GENERIC_REGISTER(dut: GENERIC_REGISTER):
         dut.clear.value = BinaryValue(clear)
         dut.enable.value = BinaryValue(enable)
         dut.source.value = BinaryValue(source)
-        cocotb.start_soon(clock.start(start_high=False))
 
         await RisingEdge(dut.clock)
-
         await Timer(Decimal(20000), units="ns")
-
-        condition = dut.destination.value.binstr == destination
-
-        if not condition:
-            dut._log.error(
-                f"Expected value: {destination} Obtained value: {dut.destination.value.binstr}"
-            )
-
-        assert (
-            condition
-        ), f"Error in test {index}: clear={clear} enable={enable} source={source} clock={clock}"
+        utils.assert_output(dut.destination, destination, f"At clock {index}.")
         await Timer(Decimal(20000), units="ns")
 
 
-def test_GENERIC_REGISTER():
-    GENERIC_REGISTER.test_with(tb_GENERIC_REGISTER)
+def test_GENERIC_REGISTER_syntesis():
+    GENERIC_REGISTER.build_vhd()
+    GENERIC_REGISTER.build_netlistsvg()
+
+
+def test_GENERIC_REGISTER_case_1():
+    GENERIC_REGISTER.test_with(tb_GENERIC_REGISTER_case_1)
 
 
 if __name__ == "__main__":
-    test_GENERIC_REGISTER()
+    pytest.main(["-k", f"test_GENERIC_REGISTER"])
