a65406adf1bb1e58cefc5cbdcc6bff50219b27fd
FelixLuciano <felix.lucianodias@gmail.com>
2024-03-17 21:48:00 -0300

    Refactor STAGE_IF entity and remove ROM

--- src/STAGE_IF.vhd
+++ src/STAGE_IF.vhd
@@ -7,45 +7,33 @@ use WORK.TOP_LEVEL_CONSTANTS.ALL;
 
 entity STAGE_IF is
 
-    generic (
-        DATA_WIDTH : natural := XLEN
-    );
-
     port (
-        clock       : in  std_logic;
-        source      : in t_IF_SIGNALS;
-        destination : out t_IF_ID_SIGNALS
+        control         : in  t_CONTROL_CLK_CLR_EN;
+        source          : in  t_CONTROL_IF;
+        address_jump    : in  t_DATA;
+        address_program : out t_DATA
     );
 
 end entity;
 
 architecture RTL of STAGE_IF is
 
-    signal enable_pc : std_logic;
-    signal pc        : std_logic_vector((DATA_WIDTH - 1) downto 0);
+    signal enable : std_logic;
 
 begin
 
     process(source.enable_stall) is
     begin
-        enable_pc <= NOT source.enable_stall;
+        enable <= control.enable AND NOT source.enable_stall;
     end process;
 
     MODULE_PC : entity WORK.MODULE_PC
         port map (
-            clock        => clock,
-            jump_address => source.source,
-            selector     => source.select_source_pc,
-            enable       => enable_pc,
-            destination  => pc
+            clock        => control.clock,
+            jump_address => address_jump,
+            selector     => source.select_source,
+            enable       => enable,
+            destination  => address_program
         );
-
-    MODULE_ROM : entity WORK.MODULE_ROM
-        port map (
-            pc           => pc,
-            destination  => destination.instruction
-        );
-
-    destination.pc <= pc;
 
 end architecture;
