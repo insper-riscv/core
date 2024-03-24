971c1835e0a85a8411f1c2635c3944350e7e377f
FelixLuciano <felix.lucianodias@gmail.com>
2024-03-24 13:38:58 -0300

    Add import statements and utility functions for wavedrom and pytest_check

--- test/utils.py
+++ test/utils.py
@@ -1,5 +1,6 @@
 from inspect import isclass
 import os
+import json
 import typing as T
 import subprocess
 from pathlib import Path
@@ -10,6 +11,10 @@ import cocotb.binary
 import cocotb.handle
 import cocotb.log
 import cocotb.runner
+import cocotb.wavedrom
+import cocotb.triggers
+import wavedrom
+import pytest_check as check
 
 
 _TESTCASE_TYPE = T.Union[
@@ -37,6 +42,36 @@ class DUT(T.Type[cocotb.handle.HierarchyObject]):
         return case.__name__
 
     @classmethod
+    def _get_input_pins(cls):
+        children: T.Set[str] = set()
+
+        for key in dir(cls):
+            if key.startswith("_"):
+                continue
+
+            value = getattr(cls, key)
+
+            if value == DUT.Input_pin:
+                children.add(key)
+
+        return children
+
+    @classmethod
+    def _get_output_pins(cls):
+        children: T.Set[str] = set()
+
+        for key in dir(cls):
+            if key.startswith("_"):
+                continue
+
+            value = getattr(cls, key)
+
+            if value == DUT.Output_pin:
+                children.add(key)
+
+        return children
+
+    @classmethod
     def _get_children(cls):
         children: T.Set[T.Type[DUT]] = set()
 
@@ -59,7 +94,7 @@ class DUT(T.Type[cocotb.handle.HierarchyObject]):
         def rename_keys(obj):
             if isinstance(obj, dict):
                 for key in list(obj.keys()):
-                    new_key = key.replace('.', ' ')
+                    new_key = key.replace(".", " ")
                     obj[new_key] = obj.pop(key)
 
                     if isinstance(obj[new_key], (dict, list)):
@@ -72,7 +107,7 @@ class DUT(T.Type[cocotb.handle.HierarchyObject]):
             design = json.load(text_file)
 
         rename_keys(design)
-            
+
         with open(filename, "w") as text_file:
             json.dump(design, text_file, indent=4)
 
@@ -101,8 +136,6 @@ class DUT(T.Type[cocotb.handle.HierarchyObject]):
             Path(filename).mkdir(exist_ok=True)
 
         entity = cls.__name__.lower()
-
-        print(True, os.getcwd())
 
         process = subprocess.Popen(
             [
@@ -158,3 +191,95 @@ def assert_output(pin: T.Type[DUT.Output_pin], value: str, message: str = ""):
     condition = pin.value.binstr == value
 
     assert condition, f"Expected {value} and obtained {pin.value.binstr}. {message}"
+
+
+def dump_waves(dut: "DUT", model: T.Optional["DUT"] = None):
+    signals = (
+        getattr(dut, key)
+        for key in dir(dut)
+        if not key.startswith("_")
+        and key != "clock"
+        and isinstance(getattr(dut, key), cocotb.handle.ModifiableObject)
+    )
+
+    return Trace(*signals, clock=dut.clock, model=model)
+
+
+def append_wavedrom(model: "DUT"):
+    def aux_1(fn):
+        async def aux(dut: "DUT"):
+            with dump_waves(dut, model) as trace:
+                await fn(dut, trace)
+                trace.write(f"../sim_build/{fn.__name__}.svg")
+
+        aux.__name__ = fn.__name__
+
+        return aux
+
+    return aux_1
+
+
+class Trace:
+    def __init__(self, *args: T.Any, clock: T.Any, model: T.Optional["DUT"] = None):
+        self._trace = cocotb.wavedrom.trace(*args, clk=clock)
+        self.model = model
+
+    def __enter__(self):
+        self._trace.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self._trace.__exit__(exc_type, exc_val, exc_tb)
+
+    def disble(self):
+        self._trace.disable()
+
+    def enable(self):
+        self._trace.enable()
+
+    async def wait(self, clock: T.Any, count: int = 1):
+        for _ in range(count):
+            await cocotb.triggers.RisingEdge(clock)
+            await cocotb.triggers.FallingEdge(clock)
+
+    async def gap(self, clock: T.Any, count: int = 1):
+        if count < 1:
+            return
+
+        self._trace.insert_gap()
+
+        self._trace.disable()
+        await self.wait(clock, count)
+        self._trace.enable()
+
+    def write(self, filename: str):
+        source = self._trace.dumpj()
+
+        if self.model is not None:
+            data = json.loads(source)
+            inputs = self.model._get_input_pins()
+            outputs = self.model._get_output_pins()
+            index = 2
+
+            data["signal"].insert(0, ["IN"])
+            data["signal"].insert(1, ["OUT"])
+
+            for signal in data["signal"][2:]:
+                if signal["name"] in inputs:
+                    data["signal"][0].append(data["signal"].pop(index))
+                elif signal["name"] in outputs:
+                    data["signal"][1].append(data["signal"].pop(index))
+                else:
+                    index += 1
+
+            if len(data["signal"]) > 2:
+                data["signal"].insert(2, {})
+
+            source = json.dumps(data)
+
+        drawing = wavedrom.render(source)
+
+        if drawing is not None:
+            return drawing.saveas(filename)
+
+        raise ValueError("Invalid Wavedrom source!")
