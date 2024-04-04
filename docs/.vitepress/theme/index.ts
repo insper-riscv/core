2127fdd81033d44b168818b8a843cdfb3a1182db
FelixLuciano <felix.lucianodias@gmail.com>
2024-04-04 20:23:04 -0300

    Add pan-zoom functionality and pan-container component to theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
+import panZoom from 'vue-panzoom'
+
+import panContainer from './components/pan-container.vue'
 import Layout from './layout.vue'
 
 import './style.css'
 
+/** @type {import('vitepress').Theme} */
 export default {
     extends: DefaultTheme,
     Layout,
+
+    enhanceApp({ app }) {
+        app.use(panZoom)
+        app.component('pan-container', panContainer)
+    }
 }
