792b9aa4da05b9600b8d96443c171fb149bdae24
gianvr <gvgiancarlo1@gmail.com>
2024-04-09 14:15:57 +0000

    stress test mux2x1

--- test/test_GENERIC_MUX_2X1.py
+++ test/test_GENERIC_MUX_2X1.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import pytest
 from cocotb.binary import BinaryValue
@@ -48,16 +49,43 @@ async def tb_GENERIC_MUX_2X1_case_1(dut: GENERIC_MUX_2X1, trace: utils.Trace):
 
 @GENERIC_MUX_2X1.testcase
 async def tb_GENERIC_MUX_2X1_case_stress(dut: GENERIC_MUX_2X1, trace: utils.Trace):
+    for _ in range(1_000_000):
+        source_1 = random.getrandbits(32)
+        source_2 = random.getrandbits(32)
+        selector = random.getrandbits(1)
+
+        source_1_bits = utils.convert_to_binstr(source_1, 32)
+        source_2_bits = utils.convert_to_binstr(source_2, 32)
+        selector_bits = utils.convert_to_binstr(selector, 1)
+    
+        dut.source_1.value = BinaryValue(source_1_bits)
+        dut.source_2.value = BinaryValue(source_2_bits)
+        dut.selector.value = BinaryValue(selector_bits)
+    
+        await trace.cycle()
+
+        message = f"source_1: {source_1_bits}, source_2: {source_2_bits}, selector: {selector_bits}"
+
+        yield trace.check(dut.destination, utils.convert_to_binstr(source_1 if selector == 0 else source_2, 32), message)
+
+@GENERIC_MUX_2X1.testcase
+async def tb_GENERIC_MUX_2X1_case_stress_5_bits(dut: GENERIC_MUX_2X1, trace: utils.Trace):
     bits = 5
     for i in range(2**bits):
         for j in range(2**bits):
             for k in range(2):
-                dut.source_1.value = BinaryValue(utils.convert_to_binstr(i, bits))
-                dut.source_2.value = BinaryValue(utils.convert_to_binstr(j, bits))
-                dut.selector.value = BinaryValue(utils.convert_to_binstr(k, 1))
+                source_1 = utils.convert_to_binstr(i, bits)
+                source_2 = utils.convert_to_binstr(j, bits)
+                selector = utils.convert_to_binstr(k, 1)
+                
+                dut.source_1.value = BinaryValue(source_1)
+                dut.source_2.value = BinaryValue(source_2)
+                dut.selector.value = BinaryValue(selector)
 
+                message = f"source_1: {source_1}, source_2: {source_2}, selector: {selector}"
+                
                 await trace.cycle()
-                yield trace.check(dut.destination, utils.convert_to_binstr(i if k == 0 else j, bits))
+                yield trace.check(dut.destination, utils.convert_to_binstr(i if k == 0 else j, bits), message)
 
 @pytest.mark.synthesis
 def test_GENERIC_MUX_2X1_synthesis():
@@ -79,8 +107,17 @@ def test_GENERIC_MUX_2X1_stress():
         [
             tb_GENERIC_MUX_2X1_case_stress,
         ],
+    )
+
+@pytest.mark.stress
+def test_GENERIC_MUX_2X1_stress_5_bits():
+    GENERIC_MUX_2X1.test_with(
+        [
+            tb_GENERIC_MUX_2X1_case_stress_5_bits,
+        ],
         parameters={"DATA_WIDTH": 5},
     )
+
 
 
 if __name__ == "__main__":
