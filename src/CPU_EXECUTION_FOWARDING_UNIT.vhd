3baf212e3a4a04135ac00f96de43f1b9ab5ff679
FelixLuciano <felix.lucianodias@gmail.com>
2024-04-15 20:03:15 -0300

    Refactor CPU_EXECUTION_FOWARDING_UNIT.vhd to use stage_ prefixes for signal names

--- src/CPU_EXECUTION_FOWARDING_UNIT.vhd
+++ src/CPU_EXECUTION_FOWARDING_UNIT.vhd
@@ -3,23 +3,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 library WORK;
-use WORK.TOP_LEVEL_CONSTANTS.ALL;
+use WORK.CPU.ALL;
 
 entity CPU_EXECUTION_FOWARDING_UNIT is
 
-    generic (
-        ADDRESS_WIDTH : natural := 5
-    );
-
     port (
-        register_source_1        : in  t_REGISTER;
-        register_source_2        : in  t_REGISTER;
-        register_destination_mem : in  t_REGISTER;
-        enable_write_mem         : in  std_logic;
-        register_destination_wb  : in  t_REGISTER;
-        enable_write_wb          : in  std_logic;
-        mux_control_1            : out  std_logic_vector(1 downto 0) := (others => '0');
-        mux_control_2            : out  std_logic_vector(1 downto 0) := (others => '0')
+        stage_ex_select_source_1     : in  t_REGISTER;
+        stage_ex_select_source_2     : in  t_REGISTER;
+        stage_mem_enable_destination : in  std_logic;
+        stage_mem_select_destination : in  t_REGISTER;
+        stage_wb_enable_destination  : in  std_logic;
+        stage_wb_select_destination  : in  t_REGISTER;
+        stage_id_select_source_1     : out std_logic_vector(1 downto 0);
+        stage_id_select_source_2     : out std_logic_vector(1 downto 0)
     );
 
 end entity;
@@ -30,13 +26,32 @@ architecture RTL of CPU_EXECUTION_FOWARDING_UNIT is
 
 begin
 
-    
-    mux_control_1 <= "10" when (enable_write_mem = '1' and register_source_1 = register_destination_mem and register_destination_mem /= "00000") else
-                     "01" when (enable_write_wb = '1' and register_source_1 = register_destination_wb and register_destination_wb /= "00000") else
-                     "00";
+    stage_id_select_source_1 <= (
+        "10" when (
+            stage_mem_enable_destination = '1' and
+            stage_ex_select_source_1 = stage_mem_select_destination and
+            stage_mem_select_destination /= "00000"
+        ) else
+        "01" when (
+            stage_wb_enable_destination = '1' and
+            stage_ex_select_source_1 = stage_wb_select_destination and
+            stage_wb_select_destination /= "00000"
+        ) else
+        "00"
+    );
 
-    mux_control_2 <= "10" when (enable_write_mem = '1' and register_source_2 = register_destination_mem and register_destination_mem /= "00000") else
-                     "01" when (enable_write_wb = '1' and register_source_2 = register_destination_wb and register_destination_wb /= "00000") else
-                     "00";
+    stage_id_select_source_2 <= (
+        "10" when (
+            stage_mem_enable_destination = '1' and
+            stage_ex_select_source_2 = stage_mem_select_destination and
+            stage_mem_select_destination /= "00000"
+        ) else
+        "01" when (
+            stage_wb_enable_destination = '1' and
+            stage_ex_select_source_2 = stage_wb_select_destination and
+            stage_wb_select_destination /= "00000"
+        ) else
+        "00"
+    );
 
 end architecture;
