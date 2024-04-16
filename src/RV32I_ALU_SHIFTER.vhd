284f8de2125d778cbe8a0aadff5d3e126bab6862
FelixLuciano <felix.lucianodias@gmail.com>
2024-04-16 16:29:18 -0300

    Refactor RV32I_ALU_SHIFTER.vhd to add signals for shift operations

--- src/RV32I_ALU_SHIFTER.vhd
+++ src/RV32I_ALU_SHIFTER.vhd
@@ -34,26 +34,57 @@ architecture RTL of RV32I_ALU_SHIFTER is
     signal shift_right_logical      : std_logic_vector((DATA_WIDTH  - 1) downto 0);
     signal shift_right_arithmetical : std_logic_vector((DATA_WIDTH  - 1) downto 0);
 
+    signal aaa : std_logic_vector((DATA_WIDTH  - 1) downto 0);
+    signal bbb : std_logic_vector((DATA_WIDTH  - 1) downto 0);
+    signal ccc : std_logic_vector((DATA_WIDTH  - 1) downto 0);
+
 begin
 
     index <=    to_integer(unsigned(shamt));
 
     sign_vec <= (others => source(DATA_WIDTH - 1));
 
+    process(source, index)
+    begin
+        aaa <= source;
+        LEFT_LOGICAL : for i in 2 to (DATA_WIDTH - 2) loop
+            if i = index then
+                exit;
+            end if;
+            aaa <= aaa(DATA_WIDTH - 2 downto 0) & '0';
+        end loop;
+
+        bbb <= source;
+        RIGHT_LOGICAL : for i in 2 to (DATA_WIDTH - 2) loop
+            if i = index then
+                exit;
+            end if;
+            bbb <= '0' & bbb(DATA_WIDTH - 1 downto 1);
+        end loop;
+
+        ccc <= source;
+        RIGHT_ARITHMETICAL : for i in 2 to (DATA_WIDTH - 2) loop
+            if i = index then
+                exit;
+            end if;
+            ccc <= source(DATA_WIDTH - 1) & ccc(DATA_WIDTH - 1 downto 1);
+        end loop;
+    end process;
+
     shift_left_logical <=   source when (shamt = LOW_SELECTOR) else
                             source(0) & LOW_VEC((DATA_WIDTH - 2) downto 0) when (shamt = HIGH_SELECTOR) else
                             source((DATA_WIDTH - 2) downto 0) & '0' when (shamt = ONE_SELECTOR) else
-                            source((DATA_WIDTH - index - 1) downto 0) & LOW_VEC((index - 1) downto 0);
+                            aaa;
 
     shift_right_logical <=  source when (shamt = LOW_SELECTOR) else
                             LOW_VEC((DATA_WIDTH - 1) downto 1) & source(DATA_WIDTH - 1) when (shamt = HIGH_SELECTOR) else
                             '0' & source((DATA_WIDTH - 1) downto 1) when (shamt = ONE_SELECTOR) else
-                            LOW_VEC((index - 1) downto 0) & source((DATA_WIDTH - 1) downto index);
+                            bbb;
 
     shift_right_arithmetical <= source when (shamt = LOW_SELECTOR) else
                                 sign_vec((DATA_WIDTH - 1) downto 1) & source(DATA_WIDTH - 1) when (shamt = HIGH_SELECTOR) else
                                 sign_vec(0) & source((DATA_WIDTH - 1) downto 1) when (shamt = ONE_SELECTOR) else
-                                sign_vec((index - 1) downto 0) & source((DATA_WIDTH - 1) downto index);
+                                ccc;
 
     destination <=  shift_left_logical       when (select_function(2 downto 0) = "100") else
                     shift_right_logical      when (select_function(2 downto 0) = "101") else
