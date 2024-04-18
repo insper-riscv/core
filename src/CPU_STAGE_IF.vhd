59ee34e715cc12ae31a29c9b0cdeb08dec196fed
FelixLuciano <felix.lucianodias@gmail.com>
2024-04-18 20:04:20 -0300

    Refactor VHDL files to update select_function width in CPU.vhd

--- src/CPU_STAGE_IF.vhd
+++ src/CPU_STAGE_IF.vhd
@@ -3,21 +3,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 library WORK;
-use WORK.CPU.ALL;
 
 entity CPU_STAGE_IF is
 
     port (
         clock           : in  std_logic;
+        clear           : in  std_logic;
         enable          : in  std_logic;
-        source          : in  t_CONTROL_IF;
-        address_jump    : in  t_DATA;
-        address_program : out t_DATA
+        source          : in  WORK.CPU.t_CONTROL_IF;
+        address_jump    : in  WORK.CPU.t_DATA;
+        address_program : out WORK.CPU.t_DATA
     );
 
 end entity;
 
-architecture RTL of CPU_STAGE_IF is
+architecture RV32I of CPU_STAGE_IF is
 
     signal enable_0 : std_logic;
 
@@ -28,12 +28,13 @@ begin
         enable_0 <= enable AND NOT source.enable_stall;
     end process;
 
-    MODULE_PROGRAM_COUNTER : entity WORK.MODULE_PROGRAM_COUNTER
+    MODULE_PROGRAM_COUNTER : entity WORK.MODULE_PROGRAM_COUNTER(RV32I)
         port map (
             clock        => clock,
-            jump_address => address_jump,
-            selector     => source.select_source,
+            clear        => clear,
             enable       => enable_0,
+            selector     => source.select_source,
+            source       => address_jump,
             destination  => address_program
         );
 
