b86bae2c685248085f63a8554576cf6c73e4076f
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-01 05:36:17 -0300

    Add GENERIC_ADDER and GENERIC_MUX_2X1 to MODULE_BRANCH_UNIT in test_MODULE_BRANCH_UNIT.py

--- test/test_MODULE_BRANCH_UNIT.py
+++ test/test_MODULE_BRANCH_UNIT.py
@@ -4,10 +4,16 @@ import pytest
 
 import utils
 from test_MODULES_package import MODULES
+from test_GENERIC_MUX_2X1 import GENERIC_MUX_2X1
+from test_GENERIC_ADDER import GENERIC_ADDER
 
 
 class MODULE_BRANCH_UNIT(utils.DUT):
     _package = MODULES
+
+    ADDER_1 = GENERIC_ADDER
+    ADDER_2 = GENERIC_ADDER
+    MUX_1 = GENERIC_MUX_2X1
 
 
 @pytest.mark.synthesis
