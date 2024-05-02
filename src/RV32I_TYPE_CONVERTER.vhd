460e67828018ab07563f69e7088800c51952d834
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-02 02:01:37 -0300

    Update RV32I_TYPE_CONVERTER.vhd to fix DATA_WIDTH generic usage

--- src/RV32I_TYPE_CONVERTER.vhd
+++ src/RV32I_TYPE_CONVERTER.vhd
@@ -4,11 +4,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 library WORK;
 
 entity RV32I_TYPE_CONVERTER is
+
+    generic (
+        DATA_WIDTH : natural := WORK.RV32I.XLEN
+    );
   
     port (
         select_type : in  std_logic_vector(2 downto 0);
-        source      : in  WORK.RV32I.t_DATA;
-        destination : out WORK.RV32I.t_DATA
+        source      : in  std_logic_vector((DATA_WIDTH - 1) downto 0);
+        destination : out std_logic_vector((DATA_WIDTH - 1) downto 0)
     );
 
 end entity;
@@ -28,7 +32,7 @@ begin
     EXTEND_BYTE: entity WORK.GENERIC_SIGNAL_EXTENDER
         generic map (
             SOURCE_WIDTH      => 8,
-            DESTINATION_WIDTH => WORK.CPU.DATA_WIDTH
+            DESTINATION_WIDTH => WORK.RV32I.XLEN
         )
         port map (
             enable_unsigned => select_type(2),
@@ -39,7 +43,7 @@ begin
     EXTEND_HALFWORD: entity WORK.GENERIC_SIGNAL_EXTENDER
         generic map (
             SOURCE_WIDTH      => 16,
-            DESTINATION_WIDTH => WORK.CPU.DATA_WIDTH
+            DESTINATION_WIDTH => WORK.RV32I.XLEN
         )
         port map (
             enable_unsigned => select_type(2),
