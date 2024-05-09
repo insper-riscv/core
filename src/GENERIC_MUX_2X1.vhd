6eaf71b7e56b8d295f536ff819d5d0204ed1cdf4
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-09 12:43:05 -0300

    Update MUX architectures to use vectorized selectors

--- src/GENERIC_MUX_2X1.vhd
+++ src/GENERIC_MUX_2X1.vhd
@@ -20,10 +20,12 @@ end entity;
 
 architecture RTL of GENERIC_MUX_2X1 is
 
-    -- No signals
+    signal selector_vec : std_logic_vector((DATA_WIDTH - 1) downto 0);
 
 begin
 
-    destination <= (source_1 AND NOT(selector)) OR (source_2 AND selector);
+    selector_vec <= (others => selector);
+
+    destination <= (source_1 AND NOT(selector_vec)) OR (source_2 AND selector_vec);
 
 end architecture;
--- src/GENERIC_MUX_4X1.vhd
+++ src/GENERIC_MUX_4X1.vhd
@@ -10,11 +10,11 @@ entity GENERIC_MUX_4X1 is
     );
 
     port (
+        selector     : in  std_logic_vector(1 downto 0);
         source_1     : in  std_logic_vector((DATA_WIDTH - 1) downto 0) := (others => '0');
         source_2     : in  std_logic_vector((DATA_WIDTH - 1) downto 0) := (others => '0');
         source_3     : in  std_logic_vector((DATA_WIDTH - 1) downto 0) := (others => '0');
         source_4     : in  std_logic_vector((DATA_WIDTH - 1) downto 0) := (others => '0');
-        selector     : in  std_logic_vector(1 downto 0);
         destination  : out std_logic_vector((DATA_WIDTH - 1) downto 0)
     );
 
@@ -22,16 +22,20 @@ end entity;
 
 architecture RTL of GENERIC_MUX_4X1 is
 
-    -- No signals
+    signal selector_0 : std_logic_vector((DATA_WIDTH - 1) downto 0);
+    signal selector_1 : std_logic_vector((DATA_WIDTH - 1) downto 0);
 
 begin
 
+    selector_0 <= (others => selector(0));
+    selector_1 <= (others => selector(1));
+
     destination <=  (
-                        (source_1 AND (NOT(selector(0)) AND NOT(selector(1)))) OR
-                        (source_2 AND (selector(0) AND NOT(selector(1))))
+                        (source_1 AND (NOT(selector_0) AND NOT(selector_1))) OR
+                        (source_2 AND (selector_0 AND NOT(selector_1)))
                     ) OR (
-                        (source_3 AND (NOT(selector(0)) AND selector(1))) OR
-                        (source_4 AND (selector(0) AND selector(1)))
+                        (source_3 AND (NOT(selector_0) AND selector_1)) OR
+                        (source_4 AND (selector_0 AND selector_1))
                     );
 
 end architecture;
