e3ee62e08739d322f0940b1b74c4d391eadcc3b6
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-09 20:09:04 -0300

    chore: Refactor GENERIC_ROM.vhd to improve readability and maintainability

--- src/GENERIC_ROM.vhd
+++ src/GENERIC_ROM.vhd
@@ -27,9 +27,43 @@ architecture RTL of GENERIC_ROM is
 
     type memory_block is array(0 TO (2**ADDRESSABLE_WIDTH - 1)) of std_logic_vector((DATA_WIDTH - 1) DOWNTO 0);
 
-    signal memory_ROM: memory_block;
-    attribute ram_init_file : string;
-    attribute ram_init_file of memory_ROM : signal is INIT_FILE;
+    impure function read_mif_file return memory_block is
+        file     file_text    : text open READ_MODE is INIT_FILE;
+        variable file_line    : line;
+        variable file_begin   : string(1 to 13);
+        variable file_address : integer := 0;
+        variable file_sep     : string(1 to 1);
+        variable result       : memory_block := (others => (others => '0'));
+    begin
+        while not endfile(file_text) loop
+            readline(file_text, file_line);
+            if file_line'length = 13 then
+                read(file_line, file_begin);
+
+                if file_begin = "CONTENT BEGIN" then
+                    exit;
+                end if;
+            end if;
+        end loop;
+
+        while not endfile(file_text) loop
+            readline(file_text, file_line);
+
+            if file_line'length = 4 then
+                exit;
+            end if;
+
+            read(file_line, file_address);
+            read(file_line, file_sep);
+            read(file_line, result(file_address));
+        end loop;
+
+        file_close(file_text);
+
+        return result;
+    end function;
+
+    signal memoryy : memory_block := read_mif_file;
 
 begin
 
@@ -38,7 +72,7 @@ begin
     begin
         index := to_integer(unsigned(address));
         if (index < (2**ADDRESSABLE_WIDTH - 1)) then
-            destination <= memory_ROM(index);
+            destination <= memoryy(index);
         else
             destination <= (others => '0');
         end if;
