d729faf919e77aeaeb6f1bb5e48c854f469f2c93
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-13 03:18:56 -0300

    refactor: Update CPU_EXECUTION_FORWARDING_UNIT to improve code readability and remove unused signals

--- src/CPU_EXECUTION_FORWARDING_UNIT.vhd
+++ src/CPU_EXECUTION_FORWARDING_UNIT.vhd
@@ -1,8 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 library WORK;
+use WORK.GENERICS.ALL;
 
 entity CPU_EXECUTION_FORWARDING_UNIT is
 
@@ -13,40 +13,35 @@ entity CPU_EXECUTION_FORWARDING_UNIT is
         stage_mem_select_destination : in  WORK.CPU.t_REGISTER;
         stage_wb_enable_destination  : in  std_logic;
         stage_wb_select_destination  : in  WORK.CPU.t_REGISTER;
-        stage_id_select_source_1     : out std_logic_vector(1 downto 0);
-        stage_id_select_source_2     : out std_logic_vector(1 downto 0)
+        select_source_1              : out std_logic_vector(1 downto 0);
+        select_source_2              : out std_logic_vector(1 downto 0)
     );
 
 end entity;
 
-architecture RTL of CPU_EXECUTION_FORWARDING_UNIT is
+architecture RV32I of CPU_EXECUTION_FORWARDING_UNIT is
 
-    -- No signals
+    constant ZERO : WORK.RV32I.t_REGISTER := (others => '0');
+
+    signal mem_source_1 : std_logic;
+    signal mem_source_2 : std_logic;
+    signal mem_zero     : std_logic;
+    signal wb_source_1  : std_logic;
+    signal wb_source_2  : std_logic;
+    signal wb_zero      : std_logic;
 
 begin
 
-    stage_id_select_source_1 <= "10" when (
-                                    stage_mem_enable_destination = '1' and
-                                    stage_ex_select_source_1 = stage_mem_select_destination and
-                                    stage_mem_select_destination /= "00000"
-                                ) else
-                                "01" when (
-                                    stage_wb_enable_destination = '1' and
-                                    stage_ex_select_source_1 = stage_wb_select_destination and
-                                    stage_wb_select_destination /= "00000"
-                                ) else
-                                "00";
+    mem_source_1 <= reduce_and(stage_ex_select_source_1 XNOR stage_mem_select_destination);
+    mem_source_2 <= reduce_and(stage_ex_select_source_2 XNOR stage_mem_select_destination);
+    wb_source_1  <= reduce_and(stage_ex_select_source_1 XNOR stage_wb_select_destination);
+    wb_source_2  <= reduce_and(stage_ex_select_source_2 XNOR stage_wb_select_destination);
+    mem_zero     <= is_equal(stage_mem_select_destination, ZERO);
+    wb_zero      <= is_equal(stage_wb_select_destination,  ZERO);
 
-    stage_id_select_source_2 <= "10" when (
-                                    stage_mem_enable_destination = '1' and
-                                    stage_ex_select_source_2 = stage_mem_select_destination and
-                                    stage_mem_select_destination /= "00000"
-                                ) else
-                                "01" when (
-                                    stage_wb_enable_destination = '1' and
-                                    stage_ex_select_source_2 = stage_wb_select_destination and
-                                    stage_wb_select_destination /= "00000"
-                                ) else
-                                "00";
+    select_source_1(0) <= stage_mem_enable_destination AND mem_source_1 AND NOT(mem_zero);
+    select_source_1(1) <= stage_wb_enable_destination  AND wb_source_1  AND NOT(wb_zero);
+    select_source_2(0) <= stage_mem_enable_destination AND mem_source_2 AND NOT(mem_zero);
+    select_source_2(1) <= stage_wb_enable_destination  AND wb_source_2  AND NOT(wb_zero);
 
 end architecture;
