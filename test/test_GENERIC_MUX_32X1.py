8d52762055eea1c1298b0aab17a60a9d8b2eff0a
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-15 20:13:08 -0300

    refactor: Update test_GENERIC_MUX_32X1.py to include coverage test case

--- test/test_GENERIC_MUX_32X1.py
+++ test/test_GENERIC_MUX_32X1.py
@@ -16,10 +16,37 @@ class GENERIC_MUX_32X1(lib.Entity):
     destination = lib.Entity.Output_pin
 
 
+@GENERIC_MUX_32X1.testcase
+async def tb_GENERIC_MUX_32X1_coverage(dut: GENERIC_MUX_32X1, trace: lib.Waveform):
+    for _ in range(5):
+        pins = [f"source_{i}" for i in range(1, 33)]
+        sources = [lib.to_binstr(random.getrandbits(8), 8) for _ in range(1, 33)]
+        choice = random.randint(0, 31)
+        selector = lib.to_binstr(choice, 5)
+        destination = sources[choice]
+
+        dut.selector.value = BinaryValue(selector)
+
+        for key, value in zip(pins, sources):
+            getattr(dut, key).value = BinaryValue(value)
+
+        await trace.cycle(1)
+
+        index = 0 #sources.index(dut.destination.value.binstr)
+        message = f"Expected {pins[choice]} but got {pins[index]}."
+
+        yield trace.check(dut.destination, destination, message)
+
+
 @pytest.mark.synthesis
 def test_GENERIC_MUX_32X1_synthesis():
     GENERIC_MUX_32X1.build_vhd()
     GENERIC_MUX_32X1.build_netlistsvg()
+
+
+@pytest.mark.coverage
+def test_GENERIC_MUX_32X1_coverage():
+    GENERIC_MUX_32X1.test_with(tb_GENERIC_MUX_32X1_coverage)
 
 
 if __name__ == "__main__":
