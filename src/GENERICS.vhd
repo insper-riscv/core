5804e57f4b463bee826876b1b3a4fbfa4b2f4ca0
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-20 02:03:22 -0300

    refactor: Fix reverse_vector function in GENERICS package

--- src/GENERICS.vhd
+++ src/GENERICS.vhd
@@ -7,36 +7,57 @@ package GENERICS is
 
     type t_std_logic_array is array(natural range <>) of std_logic_vector;
 
+    function reverse_vector (l: in std_logic_vector) return std_logic_vector;
     function reduce_or  (l : std_logic_vector) return std_logic;
     function reduce_and (l : std_logic_vector) return std_logic;
     function is_equal (l : std_logic_vector; r : std_logic_vector) return std_logic;
-    function reverse_vector (l: in std_logic_vector) return std_logic_vector;
+    function is_equal_dynamic (l : std_logic_vector; r : std_logic_vector) return std_logic;
 
 end package;
 
 package body GENERICS is
 
-    function reduce_or (l : std_logic_vector) return std_logic is
-        variable result : std_logic := '0';
+    function reverse_vector (l: in std_logic_vector) return std_logic_vector is
+        variable result : std_logic_vector(l'range);
+        alias auxiliar  : std_logic_vector(l'reverse_range) is l;
     begin
-        for i in l'reverse_range loop
-            result := result OR l(i);
+        for i in auxiliar'range loop
+            result(i) := auxiliar(i);
         end loop;
         return result;
+    end function;
+
+    function reduce_or (l : std_logic_vector) return std_logic is
+        constant mid : integer := (l'high + l'low) / 2;
+    begin
+        if l'length = 1 then
+            return l(l'low);
+        elsif l'length = 2 then
+            return l(l'low) OR l(l'high);
+        elsif l'ascending then
+            return reduce_or(l((mid + 1) to l'high)) OR reduce_or(l(l'low to mid));
+        else
+            return reduce_or(l(l'high downto (mid + 1))) OR reduce_or(l(mid downto l'low));
+        end if;
     end function;
 
     function reduce_and (l : std_logic_vector) return std_logic is
-        variable result : std_logic := '1';
+        constant mid : integer := (l'high + l'low) / 2;
     begin
-        for i in l'reverse_range loop
-            result := result AND l(i);
-        end loop;
-        return result;
+        if l'length = 1 then
+            return l(l'low);
+        elsif l'length = 2 then
+            return l(l'low) AND l(l'high);
+        elsif l'ascending then
+            return reduce_and(l((mid + 1) to l'high)) AND reduce_and(l(l'low to mid));
+        else
+            return reduce_and(l(l'high downto (mid + 1))) AND reduce_and(l(mid downto l'low));
+        end if;
     end function;
 
     function is_equal (l : std_logic_vector; r : std_logic_vector) return std_logic is
-        variable base   : std_logic_vector(r'range) := r;
-        variable result : std_logic := '1';
+        variable base : std_logic_vector(r'range) := r;
+        variable result : std_logic_vector(r'range);
     begin
         if r'ascending then
             base := reverse_vector(r);
@@ -44,22 +65,18 @@ package body GENERICS is
 
         for i in l'reverse_range loop
             if base(base'low + l'left - i) = '1' then
-                result := result AND l(i);
+                result(i) := l(i);
             else
-                result := result AND NOT(l(i));
+                result(i) := NOT(l(i));
             end if;
         end loop;
-        return result;
+
+        return reduce_and(result);
     end function;
 
-    function reverse_vector (l: in std_logic_vector) return std_logic_vector is
-        variable result: std_logic_vector(l'range);
-        alias auxiliar: std_logic_vector(l'reverse_range) is l;
+    function is_equal_dynamic (l : std_logic_vector; r : std_logic_vector) return std_logic is
     begin
-        for i in auxiliar'range loop
-            result(i) := auxiliar(i);
-        end loop;
-        return result;
+        return reduce_and(l XNOR r);
     end function;
 
 end package body;
